2932dcb00966297958fc0283b8e80445fd53cf7e
PawelMlyniec <01121788@pw.edu.pl>
2021-05-20 11:28:54 +0200

    first chromosome changes, may not work

--- amhe/evolution/agent.py
+++ amhe/evolution/agent.py
@@ -1,3 +1,18 @@
-class Agent:
+from amhe.evolution.chromosome import Chromosome
+from amhe.evolution.network import Network
+import numpy as np
+from typing import List
+
+
+class Agent:    
+    def __init__(self, population_size=10) -> None:
+        self.poputation_size = population_size
+        self.rng = np.random.default_rng(6969)  
+
+    def init_population(self, network: Network) -> List:
+        #check for pep standard layout
+        population = [Chromosome([], network, self.rng)  for _ in range(self.poputation_size)]
+        return population
+
     def do_evolution(self):
         pass
